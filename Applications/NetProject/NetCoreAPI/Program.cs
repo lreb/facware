f7b0546445b6c38c6382cd1d20e4451e2516f43b
MacBook Pro Developer <4freelancerdevelopers@gmail.com>
2019-01-28 22:42:43 -0700

    improvements to api

--- Applications/NetProject/NetCoreAPI/Program.cs
+++ Applications/NetProject/NetCoreAPI/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace NetCoreAPI
@@ -14,11 +15,20 @@ namespace NetCoreAPI
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("Seeded the database.");
+            host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+            })
+            .Build();
     }
 }
--- Applications/NetProject/NetCoreAPI/Startup.cs
+++ Applications/NetProject/NetCoreAPI/Startup.cs
@@ -5,16 +5,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace NetCoreAPI
 {
     public class Startup
     {
+        private readonly ILogger _logger;
+
+        public Startup(IConfiguration configuration, ILogger<Startup> logger)
+        {
+            Configuration = configuration;
+            _logger = logger;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // services.configure();
+            // services.conf();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            _logger.LogInformation("Added TodoRepository to services");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,13 +41,25 @@ namespace NetCoreAPI
         {
             if (env.IsDevelopment())
             {
+                _logger.LogInformation("In Development environment");
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            // app.Run(async (context) =>
+            // {
+            //     await context.Response.WriteAsync("Hello World lreb!");
+            // });
+
+            app.UseHttpsRedirection();
+            // app.UseStaticFiles();
+            // app.UseCookiePolicy();
+
+            app.UseMvc();
         }
     }
 }
